ac85cae56c1d7412ca22b402c75913105c67c1e6
unknown <fatih.zengin@ozu.edu.tr>
2022-05-25 16:29:09 +0300

    Changes in home

--- src/components/pages/Home/HomeMain.js
+++ src/components/pages/Home/HomeMain.js
@@ -10,15 +10,18 @@ export default function HomeMain() {
   return (
     <Row className="h-100">
           <Col className="my-auto text-center">
+          <h2>
+              Welcome to the Ozu Wiki
+          </h2>
+          <p>
+            Ozu Wiki aims to inform students about the Ozyegin University. It is made by students, for students.
+            You can find any relevant information about our school here. 
+            Can't find what you are looking for? You can add it by clicking <a href="">here!</a>
+          </p>
             <Button onClick={() => {
               navigate('/article/CS_391')
             }}>
               Go to article
-            </Button>
-            <Button onClick={() => {
-              wikiPageContext.setShowOffcanvas(true)
-            }}>
-              Click me
             </Button>
           </Col>
         </Row>
--- src/components/pages/Home/HomeSidebar.js
+++ src/components/pages/Home/HomeSidebar.js
@@ -25,7 +25,6 @@ export default function HomeSidebar() {
           <li key={item}>{item}</li></a>})}
       </ul>
       </div>
-    
     </>
   )
 }
