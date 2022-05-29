6a5c0737696d694b0029b2c202a5e39ce0243512
unknown <fatih.zengin@ozu.edu.tr>
2022-05-30 02:01:57 +0300

    Search main added

---
+++ src/components/pages/Search/SearchMain.js
@@ -0,0 +1,40 @@
+import { Button, Col, Row } from 'react-bootstrap';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { getArticles } from '../../../api/ArticleAPI';
+
+export default function SearchMain() {
+  const [articles, setArticles] = React.useState([]);
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+  return (
+    <Row className="h-100">
+          <Col className="my-auto text-center">
+
+          <h3>
+            Here is a list of all the articles we have:
+          </h3>
+          <ul>
+            {
+
+            React.useEffect(() => {
+              const asyncSet = async () => {
+                let articlesList = await getArticles(searchParams.get("q"));
+                articlesList.map((element) => {
+                  const item = element.name;
+                  console.log(element.name);
+                  setArticles(articles => [...articles, <a  key={item} href="" onClick={() => {navigate('/article/'+item)}} ><li>{item}</li></a>]);
+                })
+                
+              }
+              asyncSet()
+            }
+            , [])
+            }
+            {articles}
+          </ul>
+            
+          </Col>
+        </Row>
+  )
+}
